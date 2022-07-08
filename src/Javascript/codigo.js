6ce29a6723d72f0dd381df03e7e367b7ebfbbe0f
Juan Jose Bascuñan <jubascunanc@udd.cl>
2022-07-07 22:54:10 -0400

    accesos

---
+++ src/Javascript/codigo.js
@@ -0,0 +1,38 @@
+//declaracion de variables
+let equipoFavorito;
+//inicializacion de la variable
+equipoFavorito="Ajax";
+//declaracion e inicializacion de variables
+let cantidadTitulos=15;
+
+//declaracion e inicializacion de constantes
+const PI=3.1416;
+
+//Operaciones matematicas
+let edad=56;
+let edad2=23;
+
+let sumaEdades = edad + edad2; //79
+let restaEdades = edad - edad2; //33
+let multiplicacion = edad2 * 2; //46
+let division = edad / 2; //28
+
+//Concatenacion
+let nombre = "Pepe";
+let apellido = "Gomez";
+const ESPACIO=" ";
+let nombreCompleto = nombre + ESPACIO + apellido; //Pepe Gomez
+
+let textoNumero = nombre + edad; //Pepe56 es un String
+
+//consola
+console.log ("Bienvenidos a la consola");
+console.log("mi comida favorita es:" +hamburguesa);
+
+//promt
+
+let usuario= promt("Ingresa tu Usuario")
+console.log("usuario ingresado:"+usuario);
+
+//alert
+alert("usuario ingresado:"+usuario);
