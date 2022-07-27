af2fb1de8f98a99492d7af816da6044bf9308d21
Juan Jose Bascuñan <jubascunanc@udd.cl>
2022-07-26 21:10:43 -0400

    cambios java

--- src/Javascript/codigo.js
+++ src/Javascript/codigo.js
@@ -1,5 +1,6 @@
 /*consola codigo js*/
 console.log ("Bienvenido a la compra rápida");
+console.log ("Aquí puedes ordenar facilmente");
 //Concatenacion
 let nombre = prompt ("Dame tu nombre:");
 let apellido = prompt("Dame tu apellido");
@@ -29,7 +30,7 @@ if (password= "12345"){
     console.log("Bienvenido Usuario");
 break;
 }else{
-    password!="12345";
+    password!="123456";
     console.log("clave erronea, restan 3" + (3-i) + "intentos");
 }
 }
@@ -45,75 +46,66 @@ if (Edad>=18 ){
     console.log("Puedes comprar Alcohol");
 }
 
+let Alcohol=prompt("Ingresa que Alcohol deseas comprar");
+
+if (Alcohol== "Cerveza"){
+    alert("haz seleccionado la opción: Cerveza");
+    console.log("El usuario ha seleccionado la opción cerveza");
+}
+
+else if (Alcohol== "Vino"){
+    alert ("haz seleccionado la opción Vino");
+}
+
+else if (Alcohol== "Ron"){
+    alert("haz seleccionado la opción Ron");
+}
+
+else if (Alcohol== "cocktail"){
+    alert("haz seleccionado la opción Cocktail");
+}
+
+
+//break point//
 if (Edad< 18){
 alert("Eres menor de edad :(");
 console.log("NO Puedes comprar Alcohol");
 }
 
-
-    let Alcohol=prompt("Ingresa que Alcohol deseas comprar");
-
-    if (Alcohol= "Cerveza"){
-        alert("haz seleccionado la opción: Cerveza");
-    }
-
-    else if (Alcohol= "Vino"){
-        alert ("haz seleccionado la opción Vino");
-    }
-
-    else if (Alcohol= "Ron"){
-        alert("haz seleccionado la opción Ron");
-    }
-
-    else if (Alcohol= "cocktail"){
-        alert("haz seleccionado")
-    }
-
 //carrito de compras//
 
 let articulo= prompt ("ingresa los articulos que querés agregar a tu carrito: -EXIT para salir \n Hamburguesa \n Sandwiches \n Pizzas \n Ensalada \n Alcohol");
-articulo =prompt("ingrese otro pedido");
 let precio= 0;
-let precioConIva= 1.21;
+let precioConIva= 1.19;
 
 function calcular (){
 
 while (articulo!="EXIT"){
     switch (articulo){
     case "Hamburguesa":
-    case "hamburguesa":
-
-        precio= precio + 4500
-        precioConIva=precio *1,21;
-    console.log ("El valor de Hamburguesa es: $4.500 pesos");
-    console.log ("El articulo esta disponible para la Venta \n Hay stock");
-    break;
+        precio= precio + 4500;
+        console.log ("El valor de Hamburguesa es: $4.500 pesos");
+        console.log ("El articulo esta disponible para la Venta \n Hay stock");
+        break;
 
     case "Sandwiches":
-    case "sandwiches":
-        precio= precio + 2500
-        precioConIva= precio *1,21;
+        precio= precio + 2500;
         console.log("El valor de Sandwiches es: $2.500 pesos");
         console.log("El articulo esta disponible para la Venta \n Hay Stock");
         break;
 
     case "Pizzas":
-    case "pizzas":
-        precio= precio + 7000
-        precioConIva= precio *1,21;
-        console.log("El valor de Pizzas es: $7.000 pesos")
+        precio= precio + 7000;
+        console.log("El valor de Pizzas es: $7.000 pesos");
         console.log("Este articulo se encuentra en Venta \n Hay stock");
         break;
 
     case "Ensalada":
-    case "ensalada":
         console.log ("El articulo no se encuentra en Venta \n NO hay Stock");
         break;
 
     case "Alcohol":
-    case "alcohol":
-        precio= precio + 10000
-        precioConIva= precio *1,21;
+        precio= precio + 10000;
         console.log("El valor de Alcohol es: $10.000 pesos")
         console.log("El articulo se encuentra en Venta \n Hay stock");
         break;
@@ -124,9 +116,10 @@ while (articulo!="EXIT"){
     }
 articulo= prompt("ingresa los articulos que querés agregar a tu carrito: -EXIT para salir \n Hamburguesa \n Sandwiches \n Pizzas \n Ensalada \n Alcohol");
 }
-console.log("Total a Pagar $" +precio);
+let precioConIva= precio* 1.19;
+console.log("Total a Pagar $" +precioConIva);
 }
-calcular()
+calcular();
 
 //Orden de Compra//
 let eleccionMenu= prompt("¿Quieres agregar un Menú?");
@@ -154,19 +147,39 @@ switch (entrada){
     case "Si":
         alert("que bueno verte devuelta");
         console.log("Hola Usuario Antiguo");
-        continue
+        break;
     case "No":
         alert("No te reconocemos...");
         console.log ("No haz, comprado aqui antes");
-        continue
+        break;
     default:
         alert ("¿necesitas ayuda?");
         console.log("Contacta a soporte");
+        break;
     }
-let entrada = prompt("¿Ya haz comprado antes con nosotros?");
+
+entrada = prompt("¿Ya haz comprado antes con nosotros?");
 }
 
-let producto = prompt("Elija su opción de compra aquí");
+
+let inscripcion = prompt("¿NO tienes una suscripción?");
+//entra while//
+while (entrada != "Esc"){
+switch (entrada){
+    case "Quiero suscribirme":
+        alert("¿Quieres suscribirte?");
+        console.log ("suscribete para beneficios")
+        break;
+    case "No me interesa":
+        alert("No quieres ser miembro");
+        console.log ("No eres miembro");
+    default:
+        alert("...");
+}
+}
+inscripcion = prompt("¿No tienes una suscripción?");
+
+let producto = prompt("Elija su opción de despacho aquí");
 
 
 let texto="";
